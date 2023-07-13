d4968636ac375007ec9c07a27726f079c633ef0f
Juan Franco Ledesma <juanfledesma18@gmail.com>
2023-07-13 10:15:51 -0300

    end seccion 18 - forms reactivos

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,7 +1,56 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/service/validators.service';
+import { EmailValidator } from 'src/app/shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: [],
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group(
+    {
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            this.ValidatorsService.firstNameAndLastnamePattern
+          ),
+        ],
+      ],
+      // email: ['', [Validators.required, Validators.pattern(this.ValidatorsService.emailPattern)], [new EmailValidator()]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.ValidatorsService.emailPattern),
+        ],
+        [this.emailValidator],
+      ],
+      username: [
+        '',
+        [Validators.required, this.ValidatorsService.cantBeStrider],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      otherPassword: ['', [Validators.required]],
+    },
+    {
+      validators: this.ValidatorsService.isFieldOneEqualFieldTwo(
+        'password',
+        'otherPassword'
+      ),
+    }
+  );
+
+  constructor(
+    private fb: FormBuilder,
+    private ValidatorsService: ValidatorsService,
+    private emailValidator: EmailValidator
+  ) {}
+
+  isValidField(field: string) {
+    return this.ValidatorsService.isValidField(this.myForm, field);
+  }
+
+  onSubmit() {}
+}
--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
-  styles: [
-  ]
 })
 export class SwitchesPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termAndConditions: [false, Validators.requiredTrue]
+  })
 
+  constructor(private fb: FormBuilder){}
+
+  isValidFiled(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  //ngSubmit
+  onSave(){
+
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+  }
 }
